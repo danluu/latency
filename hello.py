930311865317066e6b7ec3abb770dd0185eca024
Dan Luu <danluu@gmail.com>
2014-06-26 15:46:15 -0500

    Use a template and a redirect.

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    """Homepage"""
+    return "I guess this is a homepage?"
+
+@app.route('/a-redirect')
+def a_redirect():
+    """Redirect"""
+    return redirect(url_for('a_template'))
+
+@app.route('/a-template')
+def a_template():
+    """Use a template"""
+    return render_template('template.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
